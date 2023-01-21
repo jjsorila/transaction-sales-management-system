28c4dff86a6cca90f07926341c04e01de1b43e71
JJS <jjsorila@gmail.com>
2023-01-22 04:18:26 +0800

    fixed mailer options

--- config/mail.js
+++ config/mail.js
@@ -6,11 +6,11 @@ module.exports = (email, req) => {
         port: 465,
         secure: true,
         auth: {
-            user: "opas.system@gmail.com",
-            pass: "tcwdtheeiyjbsdqg"
+            user: "berts.piggery@gmail.com",
+            pass: "fvndqgkrualotnnz"
         }
     }).sendMail({
-        from: `"Bert's Piggery" <opas.system@gmail.com>`,
+        from: `"Bert's Piggery" <berts.piggery@gmail.com>`,
         to: email,
         subject: `Reset Password`,
         html: `<b>Click this <a href="http://${req.header('host')}/reset?email=${email}">link</a> to reset password of your account.</b>`
